b8950d0eb1fa61946384c48febe0aef1e94e4301
Vikranth Reddy Tripuram (Student) <vtripura@sundevils.asu.edu>
2024-02-16 21:31:11 -0700

    Added a function to convert the dataset which filter IPhone data

--- PreProcessingCode/DataModification_IphoneandPotrait.py
+++ PreProcessingCode/DataModification_IphoneandPotrait.py
@@ -11,3 +11,81 @@ parser = argparse.ArgumentParser(description='Cutting down the data')
 parser.add_argument('--dir', help='Path to the GazeCapture dataset')
 parser.add_argument('--out_dir',  help='Path to new dataset should have image, meta folders with train, val, test subfolders in it')
 parser.add_argument('--threads', default=1, help='Number of threads', type=int)
+
+
+def convert_dataset(files, out_root):
+    for i in tqdm(files): 
+        with open(i+"/info.json") as f:
+            data = json.load(f)
+            ds = data['Dataset']
+            device = data['DeviceName']
+        if(not('iPhone' in device)):
+            continue
+        
+        expt_name = i.split('/')[-2]
+        screen_info = json.load(open(i+'/screen.json'))
+        face_det = json.load(open(i+'/appleFace.json'))
+        l_eye_det = json.load(open(i+'/appleLeftEye.json'))
+        r_eye_det = json.load(open(i+'/appleRightEye.json'))
+        dot = json.load(open(i+'/dotInfo.json'))
+        
+        portrait_orientation = np.asarray(screen_info["Orientation"])==1
+        l_eye_valid, r_eye_valid = np.array(l_eye_det['IsValid']), np.array(r_eye_det['IsValid'])
+        valid_ids = portrait_orientation*l_eye_valid*r_eye_valid
+        
+        all_dots = np.asarray(dot['DotNum'])
+        valid_dots = np.unique(all_dots[np.where(valid_ids==1)])
+        
+        try:
+            train_dots, test_dots = train_test_split(valid_dots, test_size=0.2)
+        except:
+            # Too few dots to split, skip participant
+            continue
+            
+            
+        try:
+            test_dots, val_dots = train_test_split(test_dots, test_size=0.4)
+        except:
+            val_dots = []
+        
+        split_ids = np.zeros(len(valid_ids))
+
+        for j in train_dots:
+            split_ids[all_dots==j] = 1
+
+        for j in test_dots:
+            split_ids[all_dots==j] = 2
+
+        for j in val_dots:
+            split_ids[all_dots==j] = 3
+            
+        split_ids = split_ids*valid_ids
+        
+        frame_ids = np.where(split_ids>0)[0]
+        
+        ctr = 0
+        for frame_idx in frame_ids:
+            fname = str(frame_idx).zfill(5)
+            if(split_ids[frame_idx]==1):
+                outdir = out_root+'/train/'
+            elif(split_ids[frame_idx]==2):
+                outdir = out_root+'/test/'
+            elif(split_ids[frame_idx]==3):
+                outdir = out_root+'/val/'
+            
+            meta = {}
+            meta['device'] = device
+            meta['screen_h'], meta['screen_w'] = screen_info["H"][frame_idx], screen_info["W"][frame_idx]
+            meta['face_valid'] = face_det["IsValid"][frame_idx]
+            meta['face_x'], meta['face_y'], meta['face_w'], meta['face_h'] = round(face_det['X'][frame_idx]), round(face_det['Y'][frame_idx]), round(face_det['W'][frame_idx]), round(face_det['H'][frame_idx])
+            meta['leye_x'], meta['leye_y'], meta['leye_w'], meta['leye_h'] = meta['face_x']+round(l_eye_det['X'][frame_idx]), meta['face_y']+round(l_eye_det['Y'][frame_idx]), round(l_eye_det['W'][frame_idx]), round(l_eye_det['H'][frame_idx])
+            meta['reye_x'], meta['reye_y'], meta['reye_w'], meta['reye_h'] = meta['face_x']+round(r_eye_det['X'][frame_idx]), meta['face_y']+round(r_eye_det['Y'][frame_idx]), round(r_eye_det['W'][frame_idx]), round(r_eye_det['H'][frame_idx])
+            meta['dot_xcam'], meta['dot_y_cam'] = dot['XCam'][frame_idx], dot['YCam'][frame_idx]
+            meta['dot_x_pix'], meta['dot_y_pix'] = dot['XPts'][frame_idx], dot['YPts'][frame_idx]
+
+            shutil.copy(i+'/frames/'+fname+".jpg", outdir+"/images/"+expt_name+'__'+fname+'.jpg')            
+            meta_file = outdir+'/meta/'+expt_name+'__'+fname+'.json'
+            with open(meta_file, 'w') as outfile:
+                json.dump(meta, outfile)
+            ctr+=1
+    return 0
