7d30f691086b7abef75db6f67553f22b6527f228
Chedvihas <chedvi4999@gmail.com>
2024-03-01 20:51:57 -0700

    added main for model

---
+++ models/main.py
@@ -0,0 +1,33 @@
+import math, shutil, os, time, argparse
+import numpy as np
+import scipy.io as sio
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+from ITrackerData import ITrackerData
+from ITrackerModel import ITrackerModel
+
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser(description='iTracker-pytorch-Trainer.')
+parser.add_argument('--data_path', help="Path to processed dataset. It should contain metadata.mat. Use prepareDataset.py.")
+parser.add_argument('--sink', type=str2bool, nargs='?', const=True, default=False, help="Just sink and terminate.")
+parser.add_argument('--reset', type=str2bool, nargs='?', const=True, default=False, help="Start from scratch (do not load).")
+args = parser.parse_args()
+
