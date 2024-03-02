5118b789faa640772842058b2460d2dbe6e0f89b
Vikranth Reddy Tripuram (Student) <vtripura@sundevils.asu.edu>
2024-03-01 18:57:25 -0700

    Added new class iTracker data

--- PreProcessingCode/ITrackerData.py
+++ PreProcessingCode/ITrackerData.py
@@ -38,3 +38,50 @@ class SubtractMean(object):
             Tensor: Normalized image.
         """       
         return tensor.sub(self.meanImg)
+        
+class ITrackerData(data.Dataset):
+    def __init__(self, dataPath, split = 'train', imSize=(224,224), gridSize=(25, 25)):
+
+        self.dataPath = dataPath
+        self.imSize = imSize
+        self.gridSize = gridSize
+
+        print('Loading iTracker dataset...')
+        metaFile = os.path.join(dataPath, 'metadata.mat')
+        #metaFile = 'metadata.mat'
+        if metaFile is None or not os.path.isfile(metaFile):
+            raise RuntimeError('There is no such file %s! Provide a valid dataset path.' % metaFile)
+        self.metadata = loadMetadata(metaFile)
+        if self.metadata is None:
+            raise RuntimeError('Could not read metadata file %s! Provide a valid dataset path.' % metaFile)
+
+        self.faceMean = loadMetadata(os.path.join(MEAN_PATH, 'mean_face_224.mat'))['image_mean']
+        self.eyeLeftMean = loadMetadata(os.path.join(MEAN_PATH, 'mean_left_224.mat'))['image_mean']
+        self.eyeRightMean = loadMetadata(os.path.join(MEAN_PATH, 'mean_right_224.mat'))['image_mean']
+        
+        self.transformFace = transforms.Compose([
+            transforms.Resize(self.imSize),
+            transforms.ToTensor(),
+            SubtractMean(meanImg=self.faceMean),
+        ])
+        self.transformEyeL = transforms.Compose([
+            transforms.Resize(self.imSize),
+            transforms.ToTensor(),
+            SubtractMean(meanImg=self.eyeLeftMean),
+        ])
+        self.transformEyeR = transforms.Compose([
+            transforms.Resize(self.imSize),
+            transforms.ToTensor(),
+            SubtractMean(meanImg=self.eyeRightMean),
+        ])
+
+
+        if split == 'test':
+            mask = self.metadata['labelTest']
+        elif split == 'val':
+            mask = self.metadata['labelVal']
+        else:
+            mask = self.metadata['labelTrain']
+
+        self.indices = np.argwhere(mask)[:,0]
+        print('Loaded iTracker dataset split "%s" with %d records...' % (split, len(self.indices)))
