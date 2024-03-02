3e95eb66aa264df3db3806c670a8bc4d0942b2a4
Vikranth Reddy Tripuram (Student) <vtripura@sundevils.asu.edu>
2024-03-01 19:16:42 -0700

    Added new methods for iTrackerData.py file

--- PreProcessingCode/ITrackerData.py
+++ PreProcessingCode/ITrackerData.py
@@ -85,3 +85,54 @@ class ITrackerData(data.Dataset):
 
         self.indices = np.argwhere(mask)[:,0]
         print('Loaded iTracker dataset split "%s" with %d records...' % (split, len(self.indices)))
+        
+    def loadImage(self, path):
+        try:
+            im = Image.open(path).convert('RGB')
+        except OSError:
+            raise RuntimeError('Could not read image: ' + path)
+            #im = Image.new("RGB", self.imSize, "white")
+
+        return im
+    def makeGrid(self, params):
+        gridLen = self.gridSize[0] * self.gridSize[1]
+        grid = np.zeros([gridLen,], np.float32)
+        
+        indsY = np.array([i // self.gridSize[0] for i in range(gridLen)])
+        indsX = np.array([i % self.gridSize[0] for i in range(gridLen)])
+        condX = np.logical_and(indsX >= params[0], indsX < params[0] + params[2]) 
+        condY = np.logical_and(indsY >= params[1], indsY < params[1] + params[3]) 
+        cond = np.logical_and(condX, condY)
+
+        grid[cond] = 1
+        return grid
+
+    def __getitem__(self, index):
+        index = self.indices[index]
+
+        imFacePath = os.path.join(self.dataPath, '%05d/appleFace/%05d.jpg' % (self.metadata['labelRecNum'][index], self.metadata['frameIndex'][index]))
+        imEyeLPath = os.path.join(self.dataPath, '%05d/appleLeftEye/%05d.jpg' % (self.metadata['labelRecNum'][index], self.metadata['frameIndex'][index]))
+        imEyeRPath = os.path.join(self.dataPath, '%05d/appleRightEye/%05d.jpg' % (self.metadata['labelRecNum'][index], self.metadata['frameIndex'][index]))
+
+        imFace = self.loadImage(imFacePath)
+        imEyeL = self.loadImage(imEyeLPath)
+        imEyeR = self.loadImage(imEyeRPath)
+
+        imFace = self.transformFace(imFace)
+        imEyeL = self.transformEyeL(imEyeL)
+        imEyeR = self.transformEyeR(imEyeR)
+
+        gaze = np.array([self.metadata['labelDotXCam'][index], self.metadata['labelDotYCam'][index]], np.float32)
+
+        faceGrid = self.makeGrid(self.metadata['labelFaceGrid'][index,:])
+
+        # to tensor
+        row = torch.LongTensor([int(index)])
+        faceGrid = torch.FloatTensor(faceGrid)
+        gaze = torch.FloatTensor(gaze)
+
+        return row, imFace, imEyeL, imEyeR, faceGrid, gaze
+    
+        
+    def __len__(self):
+        return len(self.indices)
