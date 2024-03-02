305bbc7fdbe6aa2a7eef731d607832a2213bac84
Chedvihas <chedvi4999@gmail.com>
2024-03-01 22:13:30 -0700

    Added code to  main

--- models/main.py
+++ models/main.py
@@ -125,3 +125,161 @@ def main():
             'best_prec1': best_prec1,
         }, is_best)
 
+
+def train(train_loader, model, criterion,optimizer, epoch):
+    global count
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+
+    for i, (row, imFace, imEyeL, imEyeR, faceGrid, gaze) in enumerate(train_loader):
+        
+        # measure data loading time
+        data_time.update(time.time() - end)
+        imFace = imFace.cuda()
+        imEyeL = imEyeL.cuda()
+        imEyeR = imEyeR.cuda()
+        faceGrid = faceGrid.cuda()
+        gaze = gaze.cuda()
+        
+        imFace = torch.autograd.Variable(imFace, requires_grad = True)
+        imEyeL = torch.autograd.Variable(imEyeL, requires_grad = True)
+        imEyeR = torch.autograd.Variable(imEyeR, requires_grad = True)
+        faceGrid = torch.autograd.Variable(faceGrid, requires_grad = True)
+        gaze = torch.autograd.Variable(gaze, requires_grad = False)
+
+        # compute output
+        output = model(imFace, imEyeL, imEyeR, faceGrid)
+
+        loss = criterion(output, gaze)
+        
+        losses.update(loss.data.item(), imFace.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        count=count+1
+
+        print('Epoch (train): [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses))
+
+def validate(val_loader, model, criterion, epoch):
+    global count_test
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    lossesLin = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+    end = time.time()
+
+
+    oIndex = 0
+    for i, (row, imFace, imEyeL, imEyeR, faceGrid, gaze) in enumerate(val_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        imFace = imFace.cuda()
+        imEyeL = imEyeL.cuda()
+        imEyeR = imEyeR.cuda()
+        faceGrid = faceGrid.cuda()
+        gaze = gaze.cuda()
+        
+        imFace = torch.autograd.Variable(imFace, requires_grad = False)
+        imEyeL = torch.autograd.Variable(imEyeL, requires_grad = False)
+        imEyeR = torch.autograd.Variable(imEyeR, requires_grad = False)
+        faceGrid = torch.autograd.Variable(faceGrid, requires_grad = False)
+        gaze = torch.autograd.Variable(gaze, requires_grad = False)
+
+        # compute output
+        with torch.no_grad():
+            output = model(imFace, imEyeL, imEyeR, faceGrid)
+
+        loss = criterion(output, gaze)
+        
+        lossLin = output - gaze
+        lossLin = torch.mul(lossLin,lossLin)
+        lossLin = torch.sum(lossLin,1)
+        lossLin = torch.mean(torch.sqrt(lossLin))
+
+        losses.update(loss.data.item(), imFace.size(0))
+        lossesLin.update(lossLin.item(), imFace.size(0))
+     
+        # compute gradient and do SGD step
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+
+        print('Epoch (val): [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Error L2 {lossLin.val:.4f} ({lossLin.avg:.4f})\t'.format(
+                    epoch, i, len(val_loader), batch_time=batch_time,
+                   loss=losses,lossLin=lossesLin))
+
+    return lossesLin.avg
+
+CHECKPOINTS_PATH = '.'
+
+def load_checkpoint(filename='checkpoint.pth.tar'):
+    filename = os.path.join(CHECKPOINTS_PATH, filename)
+    print(filename)
+    if not os.path.isfile(filename):
+        return None
+    state = torch.load(filename)
+    return state
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    if not os.path.isdir(CHECKPOINTS_PATH):
+        os.makedirs(CHECKPOINTS_PATH, 0o777)
+    bestFilename = os.path.join(CHECKPOINTS_PATH, 'best_' + filename)
+    filename = os.path.join(CHECKPOINTS_PATH, filename)
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, bestFilename)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = base_lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.state_dict()['param_groups']:
+        param_group['lr'] = lr
+
+
+if __name__ == "__main__":
+    main()
+    print('DONE')
