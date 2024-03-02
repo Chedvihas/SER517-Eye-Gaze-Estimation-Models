fb26ad63c7d269c31b29dd29d7e67d9d567ad17e
Vikranth Reddy Tripuram (Student) <vtripura@sundevils.asu.edu>
2024-03-01 17:14:31 -0700

    Created a new file iTrackerData.py with loadMetaData code

---
+++ PreProcessingCode/ITrackerData.py
@@ -0,0 +1,24 @@
+import torch.utils.data as data
+import scipy.io as sio
+from PIL import Image
+import os
+import os.path
+import torchvision.transforms as transforms
+import torch
+import numpy as np
+import re
+
+
+
+MEAN_PATH = './'
+
+def loadMetadata(filename, silent = False):
+    try:
+        # http://stackoverflow.com/questions/6273634/access-array-contents-from-a-mat-file-loaded-using-scipy-io-loadmat-python
+        if not silent:
+            print('\tReading metadata from %s...' % filename)
+        metadata = sio.loadmat(filename, squeeze_me=True, struct_as_record=False)
+    except:
+        print('\tFailed to read the meta file "%s"!' % filename)
+        return None
+    return metadata
