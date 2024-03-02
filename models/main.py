a457d03e91307ab49ec7b0e94ff13124919190ef
Chedvihas <chedvi4999@gmail.com>
2024-03-01 21:10:01 -0700

    Updated main

--- models/main.py
+++ models/main.py
@@ -31,3 +31,97 @@ parser.add_argument('--sink', type=str2bool, nargs='?', const=True, default=Fals
 parser.add_argument('--reset', type=str2bool, nargs='?', const=True, default=False, help="Start from scratch (do not load).")
 args = parser.parse_args()
 
+# Change there flags to control what happens.
+doLoad = not args.reset # Load checkpoint at the beginning
+doTest = args.sink # Only run test, no training
+
+workers = 16
+epochs = 25
+batch_size = torch.cuda.device_count()*100 # Change if out of cuda memory
+
+base_lr = 0.0001
+momentum = 0.9
+weight_decay = 1e-4
+print_freq = 10
+prec1 = 0
+best_prec1 = 1e20
+lr = base_lr
+
+count_test = 0
+count = 0
+
+
+
+def main():
+    global args, best_prec1, weight_decay, momentum
+
+    model = ITrackerModel()
+    model = torch.nn.DataParallel(model)
+    # model.cuda()
+    imSize=(224,224)
+    cudnn.benchmark = True   
+
+    epoch = 0
+    if doLoad:
+        saved = load_checkpoint()
+        if saved:
+            print('Loading checkpoint for epoch %05d with loss %.5f (which is the mean squared error not the actual linear error)...' % (saved['epoch'], saved['best_prec1']))
+            state = saved['state_dict']
+            try:
+                model.module.load_state_dict(state)
+            except:
+                model.load_state_dict(state)
+            epoch = saved['epoch']
+            best_prec1 = saved['best_prec1']
+        else:
+            print('Warning: Could not read checkpoint!')
+
+    
+    print("---------------------", args.data_path,"---------------------")
+    dataTrain = ITrackerData(dataPath = args.data_path, split='train', imSize = imSize)
+
+    dataVal = ITrackerData(dataPath = args.data_path, split='test', imSize = imSize)
+   
+    train_loader = torch.utils.data.DataLoader(
+        dataTrain,
+        batch_size=batch_size, shuffle=True,
+        num_workers=workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        dataVal,
+        batch_size=batch_size, shuffle=False,
+        num_workers=workers, pin_memory=True)
+
+
+    criterion = nn.MSELoss().cuda()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr,
+                                momentum=momentum,
+                                weight_decay=weight_decay)
+
+    # Quick test
+    if doTest:
+        validate(val_loader, model, criterion, epoch)
+        return
+
+    for epoch in range(0, epoch):
+        adjust_learning_rate(optimizer, epoch)
+        
+    for epoch in range(epoch, epochs):
+        adjust_learning_rate(optimizer, epoch)
+
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        prec1 = validate(val_loader, model, criterion, epoch)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 < best_prec1
+        best_prec1 = min(prec1, best_prec1)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+        }, is_best)
+
