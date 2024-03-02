75d8859d271b6ea7c41876f88b6974f4876e77c9
Gnanadeep-Settykara <gsettyka@asu.edu>
2024-03-01 17:19:23 -0700

    Added function for facial detection in iphones

--- PreProcessingCode/data_prep.py
+++ PreProcessingCode/data_prep.py
@@ -26,3 +26,36 @@ def crop_repeating_edge(image, rect):
     cropped_image = image[crop_y:crop_y+crop_h, crop_x:crop_x+crop_w, :]
     
     return cropped_image
+
+def load_subject(path):
+    output = {}
+    
+    # Apple Face Detections
+    with open(os.path.join(path, 'appleFace.json'), 'r') as file:
+        input_data = json.load(file)
+        output['appleFace'] = {
+            'x': [input_data['X'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['X']))],
+            'y': [input_data['Y'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['Y']))],
+            'w': [input_data['W'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['W']))],
+            'h': [input_data['H'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['H']))]
+        }
+
+    # Apple Left Eye Detections
+    with open(os.path.join(path, 'appleLeftEye.json'), 'r') as file:
+        input_data = json.load(file)
+        output['appleLeftEye'] = {
+            'x': [input_data['X'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['X']))],
+            'y': [input_data['Y'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['Y']))],
+            'w': [input_data['W'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['W']))],
+            'h': [input_data['H'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['H']))]
+        }
+
+    # Apple Right Eye Detections
+    with open(os.path.join(path, 'appleRightEye.json'), 'r') as file:
+        input_data = json.load(file)
+        output['appleRightEye'] = {
+            'x': [input_data['X'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['X']))],
+            'y': [input_data['Y'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['Y']))],
+            'w': [input_data['W'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['W']))],
+            'h': [input_data['H'][i] if input_data['IsValid'][i] else float('nan') for i in range(len(input_data['H']))]
+        }
