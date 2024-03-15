099e87a00955a0b9adedd32b79db3757932611e0
Teja Lam <tlam23@sundevils.asu.edu>
2024-03-15 15:49:53 -0700

    Updated code for FazeEstimator data

--- PreProcessingCode/FazeEstimatorData.py
+++ PreProcessingCode/FazeEstimatorData.py
@@ -20,3 +20,33 @@ norm_camera_matrix = np.array(
     ],
     dtype=np.float64,
 )
+
+
+class Undistorter:
+
+    _map = None
+    _previous_parameters = None
+
+    def __call__(self, image, camera_matrix, distortion, is_gazecapture=False):
+        h, w, _ = image.shape
+        all_parameters = np.concatenate([camera_matrix.flatten(),
+                                         distortion.flatten(),
+                                         [h, w]])
+        if (self._previous_parameters is None
+                or len(self._previous_parameters) != len(all_parameters)
+                or not np.allclose(all_parameters, self._previous_parameters)):
+            print('Distortion map parameters updated.')
+            self._map = cv.initUndistortRectifyMap(
+                camera_matrix, distortion, R=None,
+                newCameraMatrix=camera_matrix if is_gazecapture else None,
+                size=(w, h), m1type=cv.CV_32FC1)
+            print('fx: %.2f, fy: %.2f, cx: %.2f, cy: %.2f' % (
+                    camera_matrix[0, 0], camera_matrix[1, 1],
+                    camera_matrix[0, 2], camera_matrix[1, 2]))
+            self._previous_parameters = np.copy(all_parameters)
+
+        # Apply
+        return cv.remap(image, self._map[0], self._map[1], cv.INTER_LINEAR)
+
+
+undistort = Undistorter()
